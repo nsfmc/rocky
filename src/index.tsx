af05d2aebbe29e97bae5ff7c27c5c925c5994b97
Marcos Ojeda <subliminal@gmail.com>
2020-05-04 11:58:21 -0700

    gs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./Shell";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
