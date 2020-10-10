12a845533535600ec9ac134eadea2135872f854f
Marcos Ojeda <subliminal@gmail.com>
2020-10-09 21:17:33 -0700

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,37 @@ import "./App.css";
 
 type Categories = { [key: string]: any }[];
 type V0CategoryData = { categories: Categories };
-type V1CategoryData = { categories: Categories, schemaVersion: 1 };
+type V1CategoryData = {
+  categories: Categories;
+  schemaVersion: 1;
+  _rev?: string;
+};
 type CategoryData = V1CategoryData;
 
-
 interface Migration<T, U> {
-  canMigrate: (data: any) => data is T,
-  hasMigrated: (data: any) => data is U,
-  migration: (data: T) => U,
+  canMigrate: (data: any) => data is T;
+  hasMigrated: (data: any) => data is U;
+  migration: (data: T) => U;
 }
 
 let v1Migration: Migration<V0CategoryData, V1CategoryData> = {
-  canMigrate (data: any): data is V0CategoryData {
+  canMigrate(data: any): data is V0CategoryData {
     return data.categories != null && data.schemaVersion == null;
   },
-  hasMigrated (data: any): data is V1CategoryData {
-    return data.schemaVersion === 1
+  hasMigrated(data: any): data is V1CategoryData {
+    return data.schemaVersion === 1;
   },
-  migration (data: V0CategoryData): V1CategoryData {
-    return {...data, schemaVersion: 1};
+  migration(data: V0CategoryData): V1CategoryData {
+    return { ...data, schemaVersion: 1 };
   },
-}
+};
 
-function migrate<T, U>(from: T, canMigrate: (t: T) => boolean, migration: (t: T) => U): U {
-  return migration(from)
+function migrate<T, U>(
+  from: T,
+  canMigrate: (t: T) => boolean,
+  migration: (t: T) => U
+): U {
+  return migration(from);
 }
 
 const fakeData: CategoryData = {
@@ -79,30 +86,33 @@ function App() {
   //   };
   // }, []);
 
-  const persist = React.useCallback(async (newData) => {
-    console.log("persisting db");
-    if (db && db.put) {
-      const oldVersion = await db.get('categories')
-      try {
-        const nextDoc = await db.put({
-          ...oldVersion,
-          categories: newData,
-        });
-        console.log(`updated doc with version ${nextDoc.rev}`)
-      } catch (err) {
-        console.error(err);
-        console.error('failed to update the document :(')
+  const persist = React.useCallback(
+    async (newData) => {
+      console.log("persisting db");
+      if (db && db.put) {
+        const oldVersion = await db.get("categories");
+        try {
+          const nextDoc = await db.put({
+            ...oldVersion,
+            categories: newData,
+          });
+          console.log(`updated doc with version ${nextDoc.rev}`);
+        } catch (err) {
+          console.error(err);
+          console.error("failed to update the document :(");
+        }
       }
-    }
-  }, [data]);
+    },
+    [data]
+  );
 
   const [shouldPersist, setShouldPersist] = React.useState(false);
   React.useEffect(() => {
-    if (shouldPersist){ 
+    if (shouldPersist) {
       persist(data);
       setShouldPersist(false);
     }
-  }, [shouldPersist])
+  }, [shouldPersist]);
 
   const handleSave = React.useCallback(() => {
     setShouldPersist(true);
@@ -121,7 +131,7 @@ function DBLoader({
   refreshLocalData,
 }: {
   dataResource: UsePouch.Resource<CategoryData>;
-  refreshLocalData: any,
+  refreshLocalData: any;
 }) {
   let data = dataResource.read();
   // data && refreshLocalData(data.categories)
@@ -135,9 +145,12 @@ function Table({
 }: {
   resource: UsePouch.Resource<CategoryData>;
   // setData?: React.Dispatch<React.SetStateAction<Categories>>;
-  triggerSave: any,
+  triggerSave: any;
 }) {
   const [data, setData] = React.useState<CategoryData>(resource.read());
+  React.useEffect(() => {
+    console.log("somebody updated the database");
+  }, [data._rev]);
   const [schema, setSchema] = React.useState({
     mapping: {
       _id: { kind: "string", hidden: true },
@@ -163,8 +176,8 @@ function Table({
     setData(
       produce(data, (draftData) => {
         const row = data.categories.findIndex((d) => d._id === rowId);
-        if (row>-1) {
-          draftData.categories[row][columnName] = value
+        if (row > -1) {
+          draftData.categories[row][columnName] = value;
         }
       })
     );
@@ -174,12 +187,20 @@ function Table({
     const proto: { [key: string]: any } = Object.fromEntries(
       Object.entries(schema.mapping).map(([key, value]) => [
         key,
-        key === "_id" ? id : undefined,
+        key === "_id"
+          ? id
+          : value.kind === "string"
+          ? ""
+          : value.kind === "number"
+          ? 0
+          : undefined,
       ])
     );
-    setData(produce(data, draftData => {
-      draftData.categories.push(proto)
-    }));
+    setData(
+      produce(data, (draftData) => {
+        draftData.categories.push(proto);
+      })
+    );
   };
   const deleteRow = (rowId: string) => {
     const row = data.categories.findIndex((d) => d._id === rowId);
